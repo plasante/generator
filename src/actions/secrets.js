0ed9d59653012ab92cc010c15b7fb4cb11125f4e
Pierre Lasante <pierre.lasante@videotron.ca>
2023-10-01 10:08:34 -0400

    First commit

---
+++ src/actions/secrets.js
@@ -0,0 +1,4 @@
+const username = 'pierrelasante';
+const password = '321eRRe!pkarojuby';
+
+export { username, password };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
+import { fetchMemes } from './actions';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(fetchMemes());
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
